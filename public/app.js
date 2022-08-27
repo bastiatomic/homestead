e6b41bc1eab4a230d84a1804770345a218c2392c
bastiatomic <bastianh.1709@gmail.com>
2022-08-27 12:32:25 +0200

    refactor oAuth2 / GIS login

--- public/app.js
+++ public/app.js
@@ -1,3 +1,5 @@
+/* ALL SCOPS FOR GOOGLE CALENDAR AND GOOGLE SHEETS
+
 /* exported gapiLoaded */
 /* exported gisLoaded */
 /* exported handleAuthClick */
@@ -8,112 +10,50 @@ const CLIENT_ID = '837767818302-cbn24e9j41t8bfhmosgvvs200q1t991g.apps.googleuser
 const API_KEY = 'AIzaSyAlln1qR1gkv17woBHxo1Ti2TMa4zBFVjw';
 
 // Discovery doc URL for APIs used by the quickstart
-const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
+const DISCOVERY_DOC = ['https://sheets.googleapis.com/$discovery/rest?version=v4', 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'];
 
 // Authorization scopes required by the API; multiple scopes can be
 // included, separated by spaces.
-const SCOPES = 'https://www.googleapis.com/auth/spreadsheets';
+const SCOPES = 'https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/calendar';
 
 let tokenClient;
-let gapiInited = false;
-let gisInited = false;
+let client;
 
 
-/**
- * Callback after api.js is loaded.
- */
-function gapiLoaded() {
+function onLoad_gisLoaded(){
+    const client = google.accounts.oauth2.initTokenClient({
+        client_id: CLIENT_ID,
+        callback: "onTokenResponse",
+        scope: SCOPES,
+      });
+      client.requestAccessToken();
+}
+
+
+function onLoad_gapiLoaded() {
     gapi.load('client', intializeGapiClient);
 }
 
-/**
- * Callback after the API client is loaded. Loads the
- * discovery doc to initialize the API.
- */
+
 async function intializeGapiClient() {
     await gapi.client.init({
         apiKey: API_KEY,
-        discoveryDocs: [DISCOVERY_DOC],
+        discoveryDocs: DISCOVERY_DOC,
     });
-    gapiInited = true;
-    maybeEnableButtons();
+    console.log("GAPI LOADED")
 }
 
-/**
- * Callback after Google Identity Services are loaded.
- */
-function gisLoaded() {
-    tokenClient = google.accounts.oauth2.initTokenClient({
-        client_id: CLIENT_ID,
-        scope: SCOPES,
-        callback: '', // defined later
-    });
-    gisInited = true;
-    maybeEnableButtons();
-}
 
-/**
- * Enables user interaction after all libraries are loaded.
- */
-function maybeEnableButtons() {
-    if (gapiInited && gisInited) {
-    }
-}
-
-/**
- *  Sign in the user upon button click.
- */
-function handleAuthClick() {
-    tokenClient.callback = async (resp) => {
-        if (resp.error !== undefined) {
-            throw (resp);
-        }
-        document.getElementById('signout_button').style.visibility = 'visible';
-        document.getElementById('authorize_button').innerText = 'Refresh';
-        await listMajors();
-    };
-
-    if (gapi.client.getToken() === null) {
-        // Prompt the user to select a Google Account and ask for consent to share their data
-        // when establishing a new session.
-        tokenClient.requestAccessToken({
-            prompt: 'consent'
-        });
-    } else {
-        // Skip display of account chooser and consent dialog for an existing session.
-        tokenClient.requestAccessToken({
-            prompt: ''
-        });
-    }
-}
-
-/**
- *  Sign out the user upon button click.
- */
-function handleSignoutClick() {
-    const token = gapi.client.getToken();
-    if (token !== null) {
-        google.accounts.oauth2.revoke(token.access_token);
-        gapi.client.setToken('');
-        document.getElementById('content').innerText = '';
-        document.getElementById('authorize_button').innerText = 'Authorize';
-        document.getElementById('signout_button').style.visibility = 'hidden';
-    }
-}
-
-/**
- * Print the names and majors of students in a sample spreadsheet:
- * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
- */
-async function listMajors() {
+async function listMajors2() {
     let response;
     try {
         // Fetch first 10 files
         response = await gapi.client.sheets.spreadsheets.values.get({
-            spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+            spreadsheetId: '1J2eg4HmymEWdvWB462VCGUy8U2Yk1VvSYoF7hm4EXLU',
             range: 'Class Data!A2:E',
         });
     } catch (err) {
+        console.log(err)
         document.getElementById('content').innerText = err.message;
         return;
     }
@@ -127,24 +67,42 @@ async function listMajors() {
         (str, row) => `${str}${row[0]}, ${row[4]}\n`,
         'Name, Major:\n');
     document.getElementById('content').innerText = output;
-}
+
+    var event = {
+        'summary': 'Google I/O 2015',
+        'location': '800 Howard St., San Francisco, CA 94103',
+        'description': 'A chance to hear more about Google\'s developer products.',
+        'start': {
+          'dateTime': '2022-08-28T09:00:00-07:00',
+          'timeZone': 'America/Los_Angeles'
+        },
+        'end': {
+          'dateTime': '2022-08-28T17:00:00-07:00',
+          'timeZone': 'America/Los_Angeles'
+        },
+        'recurrence': [
+          'RRULE:FREQ=DAILY;COUNT=2'
+        ],
+        'attendees': [
+          {'email': 'lpage@example.com'},
+          {'email': 'sbrin@example.com'}
+        ],
+        'reminders': {
+          'useDefault': false,
+          'overrides': [
+            {'method': 'email', 'minutes': 24 * 60},
+            {'method': 'popup', 'minutes': 10}
+          ]
+        }
+      };
 
 
-function handleCredentialResponse(response) {
-    console.log("Encoded JWT ID token: " + response.credential);
-    listMajors();
-}
-window.onload = function () {
-    google.accounts.id.initialize({
-        client_id: "837767818302-cbn24e9j41t8bfhmosgvvs200q1t991g.apps.googleusercontent.com",
-        callback: handleCredentialResponse
-    });
-    google.accounts.id.renderButton(
-        document.getElementById("buttonDiv"), {
-            theme: "outline",
-            size: "large"
-        } // customization attributes
-    );
-    google.accounts.id.prompt(); // also display the One Tap dialog
-
+      var request = gapi.client.calendar.events.insert({
+        'calendarId': 'primary',
+        'resource': event
+      });
+      
+      request.execute(function(event) {
+        console.log('Event created: ' + event.htmlLink);
+      });
 }
