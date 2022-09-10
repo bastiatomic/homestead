cd8c81d1fdcc09d82ef155357bd470008a855fe5
bastiatomic <bastianh.1709@gmail.com>
2022-09-11 00:28:46 +0200

    fix for invalid scope auth

--- public/app.js
+++ public/app.js
@@ -3,70 +3,52 @@ var access_token;
 
 const CLIENT_ID = '837767818302-cbn24e9j41t8bfhmosgvvs200q1t991g.apps.googleusercontent.com';
 const API_KEY = 'AIzaSyCOcTe261vaOow-cZPbTiMkBeRANdOweeA';
-
+const ACTIVATED_GOOGLE_LOGIN = true
 const DISCOVERY_DOC = ['https://sheets.googleapis.com/$discovery/rest?version=v4', 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'];
-const SCOPES = 'https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/calendar';
+const SCOPES = 'https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/gmail.labels';
 
-function initTokenClient() {
-    tokenClient = google.accounts.oauth2.initTokenClient({
+function initTokenClient(prompt_value) {
+
+    if(ACTIVATED_GOOGLE_LOGIN){
+        tokenClient = google.accounts.oauth2.initTokenClient({
         client_id: CLIENT_ID,
         scope: SCOPES,
-        prompt: 'none',
+        prompt: prompt_value,
         callback: (tokenResponse) => {
+            //ERROR: user interaction required
             access_token = tokenResponse.access_token;
+            if (typeof tokenResponse.access_token == 'undefined'){
+                console.log("CALLING RE-ISSUE SERVICE ...")
+                setTimeout(getToken,1000)
+            }
         },
     });
     tokenClient.requestAccessToken();
     //setTimeout(listMajors, 3000) //ToDo: make requests in 1000ms intervall
 
-}
-function onLoad_gapiLoaded() {
-gapi.load('client', intializeGapiClient);
+    }
+
 }
 
+function onLoad_gapiLoaded() {
+    gapi.load('client', intializeGapiClient);
+}
 
 async function intializeGapiClient() {
-await gapi.client.init({
-apiKey: API_KEY,
-discoveryDocs: DISCOVERY_DOC,
-});
-console.log("GAPI LOADED")
-}
-function foo(){
-
-    getToken()
+    await gapi.client.init({
+    apiKey: API_KEY,
+    discoveryDocs: DISCOVERY_DOC,
+    });
+    console.log("GAPI LOADED")
 }
 
 function getToken() {
-    // Re-entrant function to request user consent.
-    // Returns an access token to the callback specified in google.accounts.oauth2.initTokenClient
-    // Use a user gesture to call this function and obtain a new, valid access token
-    // when the previous token expires and a 401 status code is returned by Google API calls.
-    tokenClient.requestAccessToken();
+
+    initTokenClient("consent")
+    console.log("RE-ISSUE TOKEN")
+    //tokenClient.requestAccessToken();
 }
 
-async function listMajors() {
-let response;
-try {
-// Fetch first 10 files
-response = await gapi.client.sheets.spreadsheets.values.get({
-spreadsheetId: '1O25tNbNDdWpgTM3tswxrfhIxcmG4DKCyVy_0vmo2rio',
-range: 'database!A1:F100',
-});
-} catch (err) {
-document.getElementById('content').innerText = err.message;
-return;
-}
-const range = response.result;
-if (!range || !range.values || range.values.length == 0) {
-document.getElementById('content').innerText = 'No values found.';
-return;
-}
-// Flatten to string to display
-const output = range.values.reduce(
-(str, row) => `${str}${row[0]},${row[1]},${row[2]},${row[3]}, ${row[4]}, ${row[5]}\n`,`\n`);
-document.getElementById('content').innerText = output;
-}
 
 /* ----------------------------------
     finance */
@@ -75,6 +57,7 @@ function finance_submit(){
     document.getElementById("submit_button").disabled = true;
     setTimeout(function(){document.getElementById("submit_button").disabled = false;},1000)
 
+    //flow: get initial object values
     var input = {
         name: document.getElementById("input_name").value,
         value: document.getElementById("input_value").value,
@@ -84,15 +67,11 @@ function finance_submit(){
         date: valid_date(document.getElementById("input_date").value),
     }
 
-    if(!valid_input(input)){
-        console.log("INPUT BUILD FAILED")
-        return
-    } else {
+    //flow: proceed if input is valid, otherwise return generic error
+    if(valid_input(input)){
         console.log("INPUT BUILD VALID")
-        console.log(input)
 
         //sending package to database
-        console.log("date: " + input.date)
         const body = {
             "values": [
               [
@@ -128,9 +107,14 @@ function finance_submit(){
                 console.log(response.result);
             });
         } catch (err) {
-            document.getElementById('content').innerText = err.message;
+            console.log("ERROR" + err.message);
             return;
         }
+       
+    } else {
+        console.log("INPUT BUILD FAILED")
+        window.alert("Some input is not valid. Kindly re-check your values.")
+        return
 
     }
 
