203e2abf77d29de0f71c76fb158b1443cb12b8ed
bastiatomic <77496632+bastiatomic@users.noreply.github.com>
2022-11-23 22:01:22 +0100

    line chart and cleanup

--- public/visitFinance.js
+++ public/visitFinance.js
@@ -7,6 +7,8 @@ function visitFinance(){
   //GET avg_month_distribution_by_budget_period
   sheetsAPI_getRows("avg_month_distribution_by_budget_period", clean_line_input, "UNFORMATTED_VALUE")
 
+  sheetsAPI_getRows("money_trend_by_start_date", finance_process_line_chart, "UNFORMATTED_VALUE")
+
 }
 
 function fetch_request_input(){
@@ -91,6 +93,49 @@ function sheetsAPI_appendRow(data, range, responseFunction){
 
 }
 
+function finance_process_line_chart(object){
+  let finance_money_history_data = object.values
+
+  let finance_money_history_data_clean = {
+      "labels": [],
+      "data": []}
+
+      finance_money_history_data.forEach(element => {
+        finance_money_history_data_clean.labels.push(js_date_to_yymmdd(new Date(element[0] * 86400000 - 2209132800000)))
+        finance_money_history_data_clean.data.push(parseInt(element[1]))
+
+    });
+
+
+  let ctx3 = document.getElementById("money_trend_by_start_date").getContext("2d")
+
+  //create new chart
+  new Chart(ctx3, add_line_chart_values("line", finance_money_history_data_clean.labels, finance_money_history_data_clean.data, "rgb(61,102,87)") )
+
+}
+
+function add_line_chart_values(type, labels_array, data_array, backgroundColor_array, options = {}){
+  return {
+    type: type,
+    data: {
+        labels: labels_array,
+        datasets: [{
+          label: "0",
+          data: data_array,
+          backgroundColor: backgroundColor_array,
+          fill: true
+        }],
+        options: options
+    }
+  }
+}
+
+function js_date_to_yymmdd(date_object){
+  var date = date_object
+  return ( ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate())) + "." +((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '.' + date.getFullYear());
+  
+  }
+
 function fetch_groups(){
 
   sheetsAPI_getRows("groups!A:C", printGroups)
@@ -134,8 +179,8 @@ function render_chart(object){
   
   let avg_month_distribution_with_leftover_by_budget_period_chart = new Chart(node, add_chart_values("pie", object.labels, object.data, object.backgroundColor) )
 
-  console.log(avg_month_distribution_with_leftover_by_budget_period_chart)
-  console.log(object)
+  //console.log(avg_month_distribution_with_leftover_by_budget_period_chart)
+  //console.log(object)
 
 }
 
@@ -143,7 +188,7 @@ function clean_line_input(object){
 
   let object2 = object.values
 
-  console.log(object2)
+  //console.log(object2)
 
   data2 = {
     "labels": [],
