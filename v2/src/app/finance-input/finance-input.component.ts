13521051aa2e76d59c676748ab3d748a64537ae8
bastiatomic <77496632+bastiatomic@users.noreply.github.com>
2023-02-22 15:03:32 +0100

    obj component

--- v2/src/app/finance-input/finance-input.component.ts
+++ v2/src/app/finance-input/finance-input.component.ts
@@ -1,3 +1,4 @@
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,37 @@ import { Component } from '@angular/core';
   templateUrl: './finance-input.component.html',
   styleUrls: ['./finance-input.component.css']
 })
+
 export class FinanceInputComponent {
+
+  notSubmitAllowed : boolean = false;
+
+  financeSubmitObject =  {
+    name: "",
+    value: 0,
+    counterpart: "",
+    category: "",
+    account: "Deutsche Bank",
+    date: new Date().toISOString().slice(0,10),
+  }
+
+  onSubmitChange(){
+    console.log("onSubmitChange")
+    console.log(this.financeSubmitObject)
+    if(this.verifyInput()){
+      this.sendToCloud()
+    }
+    
+  }
+  verifyInput(){
+    if(this.financeSubmitObject.name == ""){
+      return false;
+    }
+    return true;
+  }
+  sendToCloud(){
+    console.log("Sending to cloudService ...")
+    
+  }
 
 }
--- v2/src/app/objectives/objectives.component.ts
+++ v2/src/app/objectives/objectives.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Objectives } from '../Objectives';
+import { ObjectivesService } from '../objectives.service';
 
 @Component({
   selector: 'app-objectives',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./objectives.component.css']
 })
 export class ObjectivesComponent {
+
+  objectives : Objectives[] = []
+
+  new_objective : Objectives = {
+    id: 0,
+    name:"",
+    category:"",
+    createdAt: new Date(),
+    solvedAt: new Date()
+  }
+
+  constructor(private objs : ObjectivesService){}
+
+  ngOnInit(){
+    this.objectives = this.objs.getObjectives();
+  }
+  send_request(a : Objectives){
+    this.objectives.push(a);
+    //TODO: send to server
+  }
+  delete(obj_id : number){
+    this.objectives = this.objectives.filter(a => a.id !== obj_id)
+  }
 
 }
